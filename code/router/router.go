0e13743ae7f7bde57834b61a4241abde0f16093d
hasnatinter <hasnatinter@yandex.com>
2026-02-06 16:24:47 +0100

    feat(routing): add api versioning and healthcheck

--- code/router/router.go
+++ code/router/router.go
@@ -3,6 +3,7 @@ package router
 import (
 	"app/code/api"
 	"database/sql"
+	"net/http"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
@@ -13,7 +14,12 @@ func New(db *sql.DB) *chi.Mux {
 	r.Use(middleware.RequestID)
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
-	events := api.New(db)
-	r.Get("/events", events.GetEvents)
+	r.Get("/healthcheck", func(w http.ResponseWriter, _ *http.Request) {
+		w.Write([]byte("-"))
+	})
+	r.Route("/v1", func(r chi.Router) {
+		events := api.New(db)
+		r.Get("/events", events.GetEvents)
+	})
 	return r
 }
